34ea4660f06e91879acfc01e03acb13c20369bb4
ayoladuka <ayoladuka0@gmail.com>
2025-11-05 11:59:50 +0200

    Functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(Jamie) {
+ return 'Hello, Jamie!';
+}
+
+function add(a, b) {
+    return a + b
+}
+
+const greeting = greet("Jamie");
+const sum = add(9,10);
+
+console.log(greeting);
+console.log(sum)
